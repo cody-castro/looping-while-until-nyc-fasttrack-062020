2f0ad661d9679f0a322386b8f4f0000413a5e4be
Cody C <codyicastro@gmail.com>
2020-06-23 22:59:32 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -7,6 +7,9 @@ def using_while
   end
   
 puts "You have a levitation force of #{levitation_force}"
+
+puts using_while
+
     # puts "You have a levitation force of #{levitation_force}"
     # if levitation_force == 10
     #   break
